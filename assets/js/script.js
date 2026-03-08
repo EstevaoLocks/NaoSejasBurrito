ab2d4b41aefd483f116a699ff51d973103bdc745
EstevaoLocks <estevao.locks11@gmail.com>
2026-03-08 09:07:32 -0300

    descomentando script para scroll suave e add incons ico

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,20 @@
 // gsap.registerPlugin(ScrollToPlugin); // Pega o plugin ScrollTo do GSAP para permitir animações de rolagem suave
 
-// document.querySelectorAll("a[href^='#']").forEach(anchor => { // laço para pegar links retornados em lista
-//   anchor.addEventListener("click", function(e) {
-//     e.preventDefault();
+document.querySelectorAll("a[href^='#']").forEach(anchor => { // laço para pegar links retornados em lista
+  anchor.addEventListener("click", function(e) {
+    e.preventDefault();
 
-//     // função do GSAP para o scroll
-//     gsap.to(window, {
-//       duration: 1,
-//       scrollTo: {
-//         y: this.getAttribute("href"),
-//         offsetY: 80
-//       },
-//       ease: "power3.out"
-//     });
-//   });
-// });
+    // função do GSAP para o scroll
+    gsap.to(window, {
+      duration: 1,
+      scrollTo: {
+        y: this.getAttribute("href"),
+        offsetY: 80
+      },
+      ease: "power3.out"
+    });
+  });
+});
 
 function socialIconsAlert() {
   if (confirm("Ops! Parece que os links das redes sociais ainda não estão configurados. Mas você pode visitar um Github bem maneiro clicando em 'OK' ;D")) {
